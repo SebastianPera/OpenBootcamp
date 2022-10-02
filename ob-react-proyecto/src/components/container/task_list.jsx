50fc78abf0d864a73e985f08ae7ac23f605279f2
¨Sebastian <sebaa.pera@gmail.com>
2022-10-02 18:19:50 -0300

    task form progress

--- ob-react-proyecto/src/components/container/task_list.jsx
+++ ob-react-proyecto/src/components/container/task_list.jsx
@@ -1,88 +1,103 @@
-import React, { useState, useEffect } from "react";
-import { LEVELS } from "../../models/levels.enum";
-import { Task } from "../../models/task.class";
-import TaskComponent from "../pure/task";
+import React, { useState, useEffect } from 'react';
+import { LEVELS } from '../../models/levels.enum';
+import { Task } from '../../models/task.class'
+import TaskComponent from '../pure/task';
 
 // Importamos la hoja de estilos de task.scss
-import "../../styles/task.scss";
-import TaskForm from "../pure/forms/taskForm";
+import '../../styles/task.scss';
+import Taskform from '../pure/forms/taskForm';
 
 const TaskListComponent = () => {
-  const defaultTask1 = new Task(
-    "Example 1",
-    "Description 1",
-    false,
-    LEVELS.NORMAL
-  );
 
-  const defaultTask2 = new Task(
-    "Example 2",
-    "Description 2",
-    true,
-    LEVELS.URGENT
-  );
+    const defaultTask1 = new Task('Example1', 'Description1', true, LEVELS.NORMAL);
+    const defaultTask2 = new Task('Example2', 'Description 2', false, LEVELS.URGENT);
+    const defaultTask3 = new Task('Example3', 'Description 3', false, LEVELS.BLOCKING);
 
-  const defaultTask3 = new Task(
-    "Example 3",
-    "Description 3",
-    true,
-    LEVELS.BLOCKING
-  );
 
-  // Estado del componente
-  const [tasks, setTasks] = useState([
-    defaultTask1,
-    defaultTask2,
-    defaultTask3,
-  ]);
-  const [loading, setLoading] = useState(true);
+    // Estado del componente
+    const [tasks, setTasks] = useState([defaultTask1, defaultTask2, defaultTask3]);
+    const [loading, setLoading] = useState(true);
 
-  // Control del ciclo de vida del componente
-  useEffect(() => {
-    console.log("Task State has been modified");
-    setLoading(false);
-    return () => {
-      console.log("TaskList component is going to unmount...");
-    };
-  }, [tasks]);
+    // Control del ciclo de vida del componente
+    useEffect(() => {
+        console.log('Task State has been modified');
+        setLoading(false);
+        return () => {
+            console.log('TaskList component is going to unmount...')
+        }
+    }, [tasks])
 
-  const changeCompleted = (id) => {
-    console.log("TODO: Cambiar estado de una tarea");
-  };
 
-  return (
-    <div>
-      <div className="col-12">
-        <div className="card">
-          <div className="card-header p-3">
-            <h5>Your Tasks:</h5>
-          </div>
-          <div
-            className="card-body"
-            data-mdb-perfect-scrollbar="true"
-            style={{ position: "relative", height: "400px" }}
-          >
-            <table>
-              <thead>
-                <tr>
-                  <th scope="col">Title</th>
-                  <th scope="col">Description</th>
-                  <th scope="col">Priority</th>
-                  <th scope="col">Actions</th>
-                </tr>
-              </thead>
-              <tbody>
-                {tasks.map((task, index) => {
-                  return <TaskComponent key={index} task={task} />;
-                })}
-              </tbody>
-            </table>
-          </div>
-          <TaskForm/>
+    function completeTask(task){
+        console.log('Complete this Task:', task);
+        const index = tasks.indexOf(task);
+        const tempTasks = [...tasks];
+        tempTasks[index].completed = !tempTasks[index].completed;
+        // We update the state of the component with the new list of tasks and it will update the
+        // Iteration of the tasks in order to show the task updated
+        setTasks(tempTasks);
+    }
+
+    function deleteTask(task){
+        console.log('Detele this Task:', task);
+        const index = tasks.indexOf(task);
+        const tempTasks = [...tasks];
+        tempTasks.splice(index,1);
+        setTasks(tempTasks);
+    }
+
+    function addTask(task){
+        console.log('Detele this Task:', task);
+        const index = tasks.indexOf(task);
+        const tempTasks = [...tasks];
+        tempTasks.push(task);
+        setTasks(tempTasks);
+    }
+
+
+    return (
+        <div>
+            <div className='col-12'>
+                <div className='card'>
+                    {/* Card Header (title) */}
+                    <div className='card-header p-3'>
+                        <h5>
+                            Your Tasks:
+                        </h5>
+                    </div>
+                    {/* Card Body (content) */}
+                    <div className='card-body' data-mdb-perfect-scrollbar='true' style={ {position: 'relative', height: '400px'} }>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th scope='col'>Title</th>
+                                    <th scope='col'>Description</th>
+                                    <th scope='col'>Priority</th>
+                                    <th scope='col'>Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                { tasks.map((task, index) => {
+                                    return (
+                                            <TaskComponent 
+                                                key={index} 
+                                                task={task}
+                                                complete={completeTask}
+                                                remove = {deleteTask}
+                                            >
+                                            </TaskComponent>
+                                        )
+                                    }
+                                )}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+            <Taskform add={addTask}></Taskform>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
+
 
 export default TaskListComponent;
